310857d8131e1a7c2762f08fae53351407f1f038
cgarbs <christian.garba@gmail.com>
2022-10-16 01:27:58 +0300

    done

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import React, { Component } from 'react';
+import Home from './components/Home';
+
+class App extends Component {
+
+  // state = {
+  //   user: {}
+  // }
+
+  // async componentDidMount() {
+  //   let user = await actions.getUser()
+  //   console.log('user is ', user)
+  //   this.setState({ user })
+  // }
+
+  // setUser = (user) => {
+  //   this.setState({ user })
+  // }
+
+  render() {
+    return (
+      <main className="App">
+
+        <h1>JOURNEY</h1>
+        <Home />
+
+        {/* <h1>🧨 Iron Plate 🚀</h1>
+        <h2>{this.state.user?.email} <img src={this.state.user?.imageUrl} /></h2>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/add-post">Add Post</Link>
+          {this.state.user.email ? <Link to="/profile">Profile</Link> : <Link to="/auth"> Log In</Link>}
+
+
+        </nav>
+
+
+        <Switch>
+          <Route exact path='/' render={(props) => <Home {...props} />} />
+          <Route exact path='/add-post' render={(props) => <AddPost {...props} />} />
+          <Route exact path='/auth' render={(props) => <Auth setUser={this.setUser} {...props} />} />
+          <Route exact path='/profile' render={(props) => <Profile user={this.state.user} setUser={this.setUser} {...props} />} />
+
+        </Switch> */}
+
+
+      </main>
+    );
+  }
+}
+
+export default App;
